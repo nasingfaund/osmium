61bfa7b8777b9d6a76ff5726f77e96d3701b4d71
Kaqtus14 <kaqtus14@gmail.com>
2022-01-24 16:17:59 +0100

    Add urlbar, navigate() and clear_page()

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,7 +2,7 @@
 
 #include <QDebug>
 #include <QLabel>
-#include <QVBoxLayout>
+#include <QLineEdit>
 
 #include "node.h"
 #include "parser.h"
@@ -11,10 +11,31 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
   QWidget* widget = new QWidget(this);
   QVBoxLayout* layout = new QVBoxLayout();
 
-  QLabel* label = new QLabel("Hello Osmium!");
-  layout->addWidget(label);
+  QLineEdit* urlbar = new QLineEdit();
+  layout->addWidget(urlbar);
+
+  m_page_layout = new QVBoxLayout();
+  m_page_layout->setAlignment(Qt::AlignTop);
+  layout->addLayout(m_page_layout);
 
   widget->setLayout(layout);
   setGeometry(200, 100, 800, 600);
   setCentralWidget(widget);
+
+  navigate("");
+}
+
+void MainWindow::navigate(QString url) {
+  m_page_layout->addWidget(new QLabel("Hello Osmium! 1"));
+  m_page_layout->addWidget(new QLabel("Hello Osmium! 2"));
+}
+
+void MainWindow::clear_page() {
+  while (true) {
+    QLayoutItem* item = m_page_layout->layout()->takeAt(0);
+    if (item == NULL)
+      break;
+    delete item->widget();
+    delete item;
+  }
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QVBoxLayout>
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
  public:
-  MainWindow(QWidget *parent = nullptr);
+  MainWindow(QWidget* parent = nullptr);
+
+ private:
+  void navigate(QString url);
+  void clear_page();
+  QVBoxLayout* m_page_layout;
 };
