7ec9ec03cbf03e21a81f1536b9816178daf29b02
Kaqtus14 <kaqtus14@gmail.com>
2022-01-24 17:40:17 +0100

    Parse and render TextNodes, pass arguments

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,17 +2,15 @@
 
 #include <QDebug>
 #include <QLabel>
-#include <QLineEdit>
 
-#include "node.h"
 #include "parser.h"
 
-MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+MainWindow::MainWindow(char* argv[], QWidget* parent) : QMainWindow(parent) {
   QWidget* widget = new QWidget(this);
   QVBoxLayout* layout = new QVBoxLayout();
 
-  QLineEdit* urlbar = new QLineEdit();
-  layout->addWidget(urlbar);
+  m_urlbar = new QLineEdit();
+  layout->addWidget(m_urlbar);
 
   m_page_layout = new QVBoxLayout();
   m_page_layout->setAlignment(Qt::AlignTop);
@@ -22,7 +20,7 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
   setGeometry(200, 100, 800, 600);
   setCentralWidget(widget);
 
-  navigate("http://www.google.com");
+  navigate(argv[1]);
 }
 
 void MainWindow::navigate(QString url) {
@@ -32,17 +30,34 @@ void MainWindow::navigate(QString url) {
   request.setUrl(url);
 
   manager->get(request);
-
   connect(manager, &QNetworkAccessManager::finished, this,
           &MainWindow::handle_reply);
 }
 
 void MainWindow::handle_reply(QNetworkReply* reply) {
   if (reply->error()) {
+    // TODO: display in status bar
     qWarning() << "Error:" << reply->errorString();
     return;
   }
-  qDebug() << "Response:" << reply->readAll();
+
+  QString body = QString(reply->readAll());
+  Node root = parse(body);
+
+  m_urlbar->setText(reply->url().toString());
+  clear_page();
+  render(root);
+}
+
+void MainWindow::render(Node n) {
+  if (n.type() == NodeType::Element) {
+    for (auto c : n.children())
+      render(c);
+  } else if (n.type() == NodeType::TextNode) {
+    m_page_layout->addWidget(new QLabel(n.text()));
+  } else {
+    assert(false);
+  }
 }
 
 void MainWindow::clear_page() {
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,19 +1,23 @@
 #pragma once
-
+#include <QLineEdit>
 #include <QMainWindow>
 #include <QVBoxLayout>
 #include <QtNetwork/QtNetwork>
+
+#include "node.h"
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
  public:
-  MainWindow(QWidget* parent = nullptr);
+  MainWindow(char* argv[], QWidget* parent = nullptr);
 
  private:
   QVBoxLayout* m_page_layout;
+  QLineEdit* m_urlbar;
 
   void navigate(QString url);
   void handle_reply(QNetworkReply* reply);
+  void render(Node n);
   void clear_page();
 };
